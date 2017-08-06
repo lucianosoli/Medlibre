97258dac50095e3c449a294460cc4e8636b0815a
Luciano soli <luciano-soli@outlook.com>
2017-08-06 17:20:37 +0200

    First commit

---
+++ Medlibre.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 6 August 2017
+
+@author: Luciano soli
+
+This file is a part of Medlibre
+'''
+
+class Medlibre():
+    def __init__(self):
+        None
+        
+    def start(self):
+        print("Hello this is the start of Medlibre: 6 August 2017")
+
+if __name__ == '__main__':
+    Medlibre().start()
